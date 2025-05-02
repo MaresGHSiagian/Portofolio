3c6c02d32cc6c0bcfefa37ee77468da0198c45ed
MaresGHSiagian <marespdd@gmail.com>
2025-05-02 14:28:50 +0700

    Perbaikan

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
   return (
     <Container className="footer">
       <a href="https://maressiagian-portofolio-rosy.vercel.app/" className="logo">
-        <span>https://maressiagian-portofolio-rosy.vercel.app/</span>
+        <span>maressiagian.com</span>
       </a>
       <div>
         <p>© 2024 Mares G H Siagian</p>
